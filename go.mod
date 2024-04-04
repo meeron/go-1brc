844ae0c98f944ac8a595e7b017b49ce47bb08234
Miron Jakubowski <mijakubowski@gmail.com>
2024-04-04 22:57:53 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/meeron/go-1brc
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Data struct {
+	Min   float32
+	Sum   float32
+	Count int
+	Max   float32
+}
+
+func main() {
+	file, err := os.OpenFile("./data/measurements-rounding.txt", os.O_RDONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	result := make(map[string]*Data)
+	keys := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ";")
+		city := parts[0]
+		temp, _ := strconv.ParseFloat(parts[1], 32)
+		temp32 := float32(temp)
+
+		data, ok := result[city]
+		if !ok {
+			result[city] = &Data{
+				Min:   temp32,
+				Sum:   temp32,
+				Count: 1,
+				Max:   temp32,
+			}
+			keys = append(keys, city)
+		} else {
+			data.Count++
+			data.Sum += temp32
+
+			if data.Min > temp32 {
+				data.Min = temp32
+			}
+
+			if data.Max < temp32 {
+				data.Max = temp32
+			}
+		}
+	}
+
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		data := result[key]
+		mean := data.Sum / float32(data.Count)
+		fmt.Printf("%s;%.1f;%.1f;%.1f\n", key, data.Min, mean, data.Max)
+	}
+}
